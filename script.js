dc641e3b638b55a0718f429327b019d078db09bf
Alvaroo <gildasalvarongantung@gmail.com>
2026-02-22 15:16:29 +0700

    Refactor leaderboard and matches display logic

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ fetch("data.json")
 .then(res => res.json())
 .then(data => {
 
-let table = document.getElementById("table");
+let leaderboard = document.getElementById("leaderboard");
 
-table.innerHTML = `
+leaderboard.innerHTML = `
 <tr>
 <th>#</th>
 <th>Player</th>
@@ -16,22 +16,27 @@ table.innerHTML = `
 data.players
 .sort((a,b)=>b.points-a.points)
 .forEach((p,i)=>{
-let row = table.insertRow();
+let row = leaderboard.insertRow();
 row.innerHTML = `
-<td>${i+1}</td>
+<td><span class="rank">${i+1}</span></td>
 <td>${p.name}</td>
 <td>${p.points}</td>
 <td>${p.played}</td>
 `;
 });
 
-let matchBox = document.getElementById("matches");
+let matchesBox = document.getElementById("matches");
 
 data.matches.forEach(m=>{
 let div = document.createElement("div");
-div.className="match";
-div.innerHTML = `${m.p1} ${m.s1} - ${m.s2} ${m.p2}`;
-matchBox.appendChild(div);
+div.className = "match";
+
+div.innerHTML = `
+<span class="table">Table ${m.table}</span>
+<span>${m.p1} ${m.s1} - ${m.s2} ${m.p2}</span>
+`;
+
+matchesBox.appendChild(div);
 });
 
 });
